3e4ef742d5a077ee19b96d47f7669805a0aeb849
shumm <kiue20002001@yahoo.co.jp>
2014-11-21 12:03:14 +0900

    BackGround

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#include "DanDan_First.h"
 
 USING_NS_CC;
 
@@ -27,7 +28,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+//    auto scene = HelloWorld::createScene();
+    auto scene = DanDan::createScene();
 
     // run
     director->runWithScene(scene);
